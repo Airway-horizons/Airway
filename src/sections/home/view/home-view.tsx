70357ecb903fd2a4c9873757e694b13301adb969
Sherin Shaju <sherinshaju80@gmail.com>
2024-10-01 21:38:40 -0700

    fix

--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -257,7 +257,7 @@ export default function HomeView() {
 
         <HomePricing />
 
-        <InstagramGallery />
+        {/* <InstagramGallery /> */}
         {/* <HomeLookingFor /> */}
 
         {/* <HomeAdvertisement /> */}
